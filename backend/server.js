bba377a9230a807e1051e266b00344ee94487e2c
tal_ubuntu <tal2kk@gmail.com>
2019-04-09 14:13:14 +0300

    test server

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+router.route('/test').get((req, res) => {
+res.status(200).send('hakaton test!');
+}
+
+
+app.use('/', router);
+app.listen(3000, () => console.log('Server running on 4000'))
